923df01649b77d2397bc372be2be953e5ac2bbe6
007tickooayush <ayush.tickoo292000@gmail.com>
2024-05-24 13:13:23 +0530

    dependencies added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+// use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>>{
+    const PORT: i32 = 4040;
+
+    
+    let app: Router = Router::new().route("/", get(|| async { "Server Running" }));
+    
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}",PORT)).await?;
+    
+    
+    println!("Server running on port: {}", PORT);
+
+    axum::serve(listener, app.into_make_service()).await.unwrap();
+    Ok(())
 }
